073b17150dee92f3f4271c72e21beb9409ebeee9
Humble689 <marktravis689@gmail.com>
2025-05-08 11:50:03 +0300

    migration

--- store/models.py
+++ store/models.py
@@ -80,6 +80,7 @@ class CartItem(models.Model):
 class address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
     customer =models.ForeignKey(Customer, on_delete=models.CASCADE)
     
 
